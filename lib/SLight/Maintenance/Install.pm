4ed5f2404be1970bb9233cb5aa953a742f7b66fa
Bartłomiej Syguła <natanael@natanael.krakow.pl>
2010-06-18 00:16:26 +0200

    Missing header added (this time relally).

--- lib/SLight/Maintenance/Install.pm
+++ lib/SLight/Maintenance/Install.pm
@@ -1,4 +1,16 @@
 package SLight::Maintenance::Install;
+################################################################################
+# 
+# SLight - Lightweight Content Manager System.
+#
+# Copyright (C) 2010 Bartłomiej /Natanael/ Syguła
+#
+# This is free software.
+# It is licensed, and can be distributed under the same terms as Perl itself.
+#
+# More information on: http://slight-cms.org/
+# 
+################################################################################
 use strict; use warnings; # {{{
 
 our $VERSION = 0.0.1;
