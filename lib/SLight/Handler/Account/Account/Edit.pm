12978049e6b8a5dbf9fbc36958e1d259bf45254b
Bartłomiej Syguła <natanael@natanael.krakow.pl>
2010-11-12 22:26:04 +0100

    Skeletons for User-related modules.

---
+++ lib/SLight/Handler/Account/Account/Edit.pm
@@ -0,0 +1,21 @@
+package SLight::Handler::Account::Account::Edit;
+################################################################################
+# 
+# SLight - Lightweight Content Manager System.
+#
+# Copyright (C) 2010 Bartłomiej /Natanael/ Syguła
+#
+# This is free software.
+# It is licensed, and can be distributed under the same terms as Perl itself.
+#
+# More information on: http://slight-cms.org/
+# 
+################################################################################
+use strict; use warnings; # {{{
+
+# }}}
+
+
+
+# vim: fdm=marker
+1;
---
+++ lib/SLight/Handler/Error/InternalError/Overview.pm
@@ -0,0 +1,21 @@
+package SLight::Handler::Error::InternalError::Overview;
+################################################################################
+# 
+# SLight - Lightweight Content Manager System.
+#
+# Copyright (C) 2010 Bartłomiej /Natanael/ Syguła
+#
+# This is free software.
+# It is licensed, and can be distributed under the same terms as Perl itself.
+#
+# More information on: http://slight-cms.org/
+# 
+################################################################################
+use strict; use warnings; # {{{
+
+# }}}
+
+
+
+# vim: fdm=marker
+1;
